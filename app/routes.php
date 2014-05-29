f18f8d0a6d87e9d33e239208edc5629892acfb0f
Nicolas Silva <nsilvah@gmail.com>
2014-05-29 17:57:18 -0400

    Se evita error en el home

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-//Route::controller('/','HomeController');
+Route::controller('/','HomeController');
 Route::controller('/backend/auth','AuthController');
 
 Route::group(array('before' => 'auth'), function()
